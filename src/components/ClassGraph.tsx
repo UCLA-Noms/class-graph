29bc479e142231920ccc7af763a5b64993d44f11
henrywangzh <henrywang3@g.ucla.edu>
2023-05-30 11:53:26 -0700

    more updates to graph

--- src/components/ClassGraph.tsx
+++ src/components/ClassGraph.tsx
@@ -1,48 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Graph from 'react-graph-vis';
 
 export default function ClassGraph() {
-  const graph = {
-    nodes: [
-      { id: 1, label: 'Node 1' },
-      { id: 2, label: 'Node 2' },
-      { id: 3, label: 'Node 3' },
-      { id: 4, label: 'Node 4' },
-      { id: 5, label: 'Node 5' },
-    ],
-    edges: [
-      { from: 1, to: 2 },
-      { from: 1, to: 3 },
-      { from: 2, to: 4 },
-      { from: 2, to: 5 },
-    ],
-  };
+  const [state, setState] = useState({
+    graph: {
+      nodes: [
+        { id: 1, label: 'CS31' },
+        { id: 2, label: 'CS32' },
+        { id: 3, label: 'CS33' },
+        { id: 4, label: 'CS35L' },
+        { id: 5, label: 'PHYS1A' },
+      ],
+      edges: [
+        { from: 1, to: 2 },
+        { from: 2, to: 3 },
+        { from: 2, to: 4 },
+      ],
+    },
+    options: {
+      interaction: {
+        navigationButtons: true,
+      },
+      nodes: {
+        shape: 'dot',
+        size: 32,
+      },
+      edges: {
+        color: '#000000',
+      },
+      height: '100%',
+      width: '100%',
+    },
+    events: {
+      select: ({ nodes, edges }) => {
+        if (nodes.length > 0) {
+          alert('Selected node: ' + nodes);
+        }
+      },
+      doubleClick: ({ pointer: { canvas } }) => {
+        alert('double click! at ' + canvas.x + ', ' + canvas.y);
+      },
+    },
+  });
 
-  const options = {
-    interaction: {
-      navigationButtons: true,
-    },
-    nodes: {
-      shape: 'dot',
-      size: 32,
-    },
-    edges: {
-      color: '#000000',
-    },
-    height: '100%',
-    width: '100%',
-  };
-
-  const events = {
-    select: ({ nodes, edges }) => {
-      if (nodes.length > 0) {
-        alert('Selected node: ' + nodes);
-      }
-    },
-    doubleClick: ({ pointer: { canvas } }) => {
-      alert('double click! at ' + canvas.x + ', ' + canvas.y);
-    },
-  };
+  const { graph, options, events } = state;
 
   return <Graph graph={graph} options={options} events={events} />;
 }
