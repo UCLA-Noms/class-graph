2f1e9757fb285f904d0f070293c55cbc1c230093
jason2020 <jtay20@punahou.edu>
2023-11-28 15:01:19 -1000

    Add many recommended security headers

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,29 @@
 import React, { StrictMode } from 'react';
 import './index.css';
 import { createRoot } from 'react-dom/client';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <HelmetProvider>
+    <Helmet>
+      <meta
+        httpEquiv="Content-Security-Policy"
+        content={`
+                  default-src 'none';
+                  script-src 'self';
+                  connect-src 'self';
+                  img-src 'self';
+                  style-src 'self';
+                  frame-ancestors 'self';
+                  form-action 'self';
+                  require-trusted-types-for 'script';
+                `}
+      ></meta>
+    </Helmet>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HelmetProvider>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,20 @@ export default defineConfig({
   server: {
     open: true,
     port: 8000,
+    headers: {
+      // https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html
+      'X-Frame-Options': 'DENY', // Stops your site being used as an iframe
+      'X-XSS-Protection': '0', // XSS protection can create vulnerabilities, so we disable it
+      'X-Content-Type-Options': 'nosniff', // Block MIME type sniffing
+      'Referrer-Policy': 'strict-origin-when-cross-origin', // Protects from improper referrer data being sent
+      // Adds HSTS options to your website, with a expiry time of 1 day
+      // 'Strict-Transport-Security': 'max-age=86400; includeSubDomains',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Resource-Policy': 'same-site',
+      'Permissions-Policy': 'geolocation=(), camera=(), microphone=()', // TODO: Adjust if we need to use these
+      'X-DNS-Prefetch-Control': 'on', // Not aware of any third party DNS lookups we need to do
+    },
   },
   base: '/class-graph/',
 });
