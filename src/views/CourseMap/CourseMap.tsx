f039a94560a5b0f3eef0fb6615c9200c85692f58
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-01-04 21:13:45 -0800

    inline style

--- src/views/CourseMap/CourseMap.tsx
+++ src/views/CourseMap/CourseMap.tsx
@@ -1,15 +1,10 @@
 import { useState } from 'react';
+import { css } from '@emotion/react';
 
 import CourseGraph from './components/CourseGraph';
-import DarsPanel from 'panels/DarsPanel';
 import CourseDetails from 'panels/Inspector/CourseDetails';
+import DarsPanel from 'panels/DarsPanel';
 import { Course } from 'types/Course';
-import {
-  CourseMapClassName,
-  DarsPanelClassName,
-  InspectorPanelClassName,
-  MainPanelClassName,
-} from './style';
 
 export const CourseMap: React.FC = () => {
   const [selectedCourse, setSelectedCourse] = useState<Course | null>(null);
@@ -72,11 +67,11 @@ export const CourseMap: React.FC = () => {
   };
 
   return (
-    <div className={CourseMapClassName}>
-      <div className={DarsPanelClassName}>
+    <div css={style.courseMap}>
+      <div css={style.darsPanel}>
         <DarsPanel />
       </div>
-      <div className={MainPanelClassName}>
+      <div css={style.mainPanel}>
         <CourseGraph
           courses={courses}
           onCourseClick={handleCourseClick}
@@ -84,7 +79,7 @@ export const CourseMap: React.FC = () => {
         ></CourseGraph>
       </div>
       {selectedCourse ? (
-        <div className={InspectorPanelClassName}>
+        <div css={style.inspectorPanel}>
           <CourseDetails course={selectedCourse}></CourseDetails>
         </div>
       ) : null}
@@ -93,3 +88,22 @@ export const CourseMap: React.FC = () => {
 };
 
 export default CourseMap;
+
+const style = {
+  courseMap: css({
+    width: '100vw',
+    height: '100vh',
+    maxHeight: 'fill-available',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+  }),
+  mainPanel: css({
+    minWidth: '50%',
+    minHeight: '80%',
+    backgroundColor: 'green',
+  }),
+  darsPanel: css({ width: '20%', backgroundColor: 'red' }),
+  inspectorPanel: css({ width: '30%', backgroundColor: 'rgb(0 183 255)' }),
+};
