750586a06d064261a250df304ec5c833a69f0e8d
Justin Sheu <justinsheu3341@gmail.com>
2024-01-25 20:38:19 -0800

    feat: finished basic endpoints for course/prereq categorization

--- backend/app.js
+++ backend/app.js
@@ -42,19 +42,20 @@ app.get("/getcourses/:department", async (req, res) => {
 app.get("/getcoursesbycat/:major/:category", async (req, res) => {
     let course_result = await session.run(
         `
-        MATCH (:Category {Major: "${req.params.major}", Name: "${req.params.category}"})-[edge]->(node) RETURN node
+        MATCH (:Category {Major: "${req.params.major}", Name: "${req.params.category}"})-[edge]->(node) OPTIONAL MATCH (node)<-[edge2]-(l:Lecture) RETURN node, l
         `
     )
+    console.log(course_result.records[0])
     let prereq_result = await session.run(
         `
-        MATCH (:Category {Major: "${req.params.major}", Name: "${req.params.category}"})-[edge]->(c1:Course)-[:IS_PREREQ_OF]->(c2:Course) RETURN c1.CourseID, c1.Name, c2.CourseID, c2.Name
+        MATCH (:Category {Major: "${req.params.major}", Name: "${req.params.category}"})-[edge]->(c1:Course)-[:IS_PREREQ_OF]->(c2:Course) RETURN c1, c2
         `
     )
     res.send(
-        {
-            "course_nodes": course_result.records.filter(record => record._fields[0].labels[0] === "Course").map(record => [record._fields[0].properties.CourseID, record._fields[0].properties.Name]),
+        { // TODO: Figure out how to map course to multiple professors
+            "course_nodes": course_result.records.filter(record => record._fields[0].labels[0] === "Course").map(record => ({"class_code": record._fields[0].properties.CourseID, "class_name": record._fields[0].properties.Name, "class_description": record._fields[0].properties.Description, "class_units": record._fields[0].properties.Units, "professors": course_result.records.filter(r => (r._fields[0].labels[0] === "Course" && r._fields[0].properties.CourseID === record._fields[0].properties.CourseID)).map(record => record._fields[1].properties.Professor), "id": record._fields[0].identity.low})),
             "subcat_nodes": course_result.records.filter(record => record._fields[0].labels[0] === "Category").map(record => record._fields[0].properties.Name),
-            "prereq_edges": prereq_result.records.map(record => ({"prereq": [record._fields[0], record._fields[1]],"postreq": [record._fields[2], record._fields[3]]}))
+            "prereq_edges": prereq_result.records.map(record => ({"prereq": record._fields[0].identity.low,"postreq": record._fields[1].identity.low}))
         }    
             );
 })
