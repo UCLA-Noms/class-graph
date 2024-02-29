ca367622fcff777eeb72736eee253143aba783d2
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-02-29 13:32:27 -0800

    remove divider

--- src/panels/DarsPanel/DarsPanel.tsx
+++ src/panels/DarsPanel/DarsPanel.tsx
@@ -1,15 +1,16 @@
-import { Divider, Accordion, AccordionItem } from '@nextui-org/react';
+import { Accordion, AccordionItem } from '@nextui-org/react';
 
 export const DarsPanel: React.FC = () => {
   return (
-    <div className="w-[16rem] h-full flex flex-col relative">
-      <div className="inline-flex flex-col mx-5 my-3 gap-2">
-        <div>MAJOR REQUIREMENTS</div>
-        <div>MINOR REQUIREMENTS</div>
-        <div>ENFORCED CO-REQ/SERIES</div>
-        <div>RECCOMENDED SERIES</div>
+    <div className="w-[16rem] h-full flex flex-col relative overflow-y-auto">
+      <div className="border-b-1.5 border-gray-400">
+        <div className="inline-flex flex-col mx-5 my-3 gap-2 ">
+          <div>MAJOR REQUIREMENTS</div>
+          <div>MINOR REQUIREMENTS</div>
+          <div>ENFORCED CO-REQ/SERIES</div>
+          <div>RECCOMENDED SERIES</div>
+        </div>
       </div>
-      <Divider className="h-[1.5px]" />
       <div className="mx-5 mt-5 mb-2">
         <p>College of Letters and Sciences</p>
         <Accordion isCompact selectionMode="multiple" fullWidth>
